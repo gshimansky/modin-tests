9fc848a363b8a9b0055e3571dcc3a7e6ad6c4b12
Gregory Shimansky <gregory.shimansky@intel.com>
2021-05-04 14:10:57 -0500

    Added new groupby test

---
+++ groupby_test28.py
@@ -0,0 +1,21 @@
+#import pandas as pd
+import modin.pandas as pd
+
+x = pd.DataFrame(
+    {"id2": ["id056", "id075", "id077", "id072", "id010"],
+     "id4": [82, 30, 40, 92, 34],
+     "v1": [4, 1, 5, 1, 5],
+     "v2": [3, 2, 3, 3, 2]
+    }
+)
+gb = x.groupby(['id2','id4', "aaa"], observed=True)
+print(gb)
+print("groups = ", gb.groups)
+print("len(groups) = ", len(gb.groups))
+print("indices = ", gb.indices)
+
+gb1 = gb[["v1", "v2", "v3"]]
+print(gb1)
+print("groups = ", gb1.groups)
+print("len(groups) = ", len(gb1.groups))
+print("indices = ", gb1.indices)
